c1f12c225826a0afaccb682a573784cffdf8cf8a
Dylan3009 <dylan.melendezl@hotmail.com>
2023-04-15 21:09:38 -0600

    Add: New responses for bot lemmatizer

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -5,13 +5,18 @@ from nltk.stem import WordNetLemmatizer
 nltk.download('punkt')
 nltk.download('wordnet')
 
-saludos = ['Hola!', '¡Buen día!', '¡Hola!']
-preguntas_rutinas = ['¿Qué tipo de rutina buscas?']
+saludos = ['Hola!', '¡Buen día!', '¡Hola!', '¡Hola, como estas?', '¡Hola, que tal?', '¡Hola, que tal te va?']
+preguntas_rutinas = ['¿Qué tipo de rutina buscas?', '¿Quieres una rutina para principiantes o avanzados?', '¿Buscas una rutina de fuerza o de cardio?']
 preguntas_dietas = ['¿Qué tipo de dieta buscas?', '¿Quieres una dieta vegetariana o vegana?', '¿Buscas una dieta baja en grasas o en carbohidratos?']
 despedidas = ['¡Hasta luego!', '¡Adiós!', '¡Que tengas un buen día!']
 ayudas = ['Como puedo ayudarte?', 'Con gusto te ayudare con lo que necesites', 'Cuentame que necesitas']
 nombres = ['Como modelo de aprendizaje creado por el grupo 16 aun no tengo un nombre, pero en cuanto lo tenga te lo dire']
 sugerencias = ['¿Prefieres entrenamiento cardiovascular o de fuerza?', '¿Buscas una rutina de principiantes o avanzada?']
+rutinas = {
+    'Principiantes': ['Entrenamiento de cuerpo completo para principiantes', 'Rutina de cardio para principiantes'],
+    'Intermedios': ['Entrenamiento de fuerza para intermedios', 'Rutina de yoga para intermedios'],
+    'Avanzados': ['Entrenamiento de alta intensidad para avanzados', 'Rutina de pilates para avanzados']
+}
 
 def procesar_entrada(entrada):
     lematizador = WordNetLemmatizer()
