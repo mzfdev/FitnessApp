840aec4c69351402d71a6ce5b1b0062e7afae609
Dylan3009 <dylan.melendezl@hotmail.com>
2023-04-15 21:19:22 -0600

    Add: New responses for bot lemmatizer

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -6,17 +6,21 @@ nltk.download('punkt')
 nltk.download('wordnet')
 
 saludos = ['Hola!', '¡Buen día!', '¡Hola!', '¡Hola, como estas?', '¡Hola, que tal?', '¡Hola, que tal te va?']
-preguntas_rutinas = ['¿Qué tipo de rutina buscas?', '¿Quieres una rutina para principiantes o avanzados?', '¿Buscas una rutina de fuerza o de cardio?']
+preguntas_rutinas = ['¿Qué tipo de rutina buscas principiantes o avanzados?', '¿Quieres una rutina para principiantes o avanzados?', '¿Buscas una rutina de fuerza o de cardio?', '¿Qué tipo de rutina buscas fuerza o cardio?']
 preguntas_dietas = ['¿Qué tipo de dieta buscas?', '¿Quieres una dieta vegetariana o vegana?', '¿Buscas una dieta baja en grasas o en carbohidratos?']
-despedidas = ['¡Hasta luego!', '¡Adiós!', '¡Que tengas un buen día!']
-ayudas = ['Como puedo ayudarte?', 'Con gusto te ayudare con lo que necesites', 'Cuentame que necesitas']
+despedidas = ['¡Hasta luego!', '¡Adiós!', '¡Que tengas un buen día!', '¡Hasta pronto!']
+ayudas = ['Como puedo ayudarte?', 'Con gusto te ayudare con lo que necesites', 'Cuentame que necesitas', '¿En que te puedo ayudar?']
 nombres = ['Como modelo de aprendizaje creado por el grupo 16 aun no tengo un nombre, pero en cuanto lo tenga te lo dire']
-sugerencias = ['¿Prefieres entrenamiento cardiovascular o de fuerza?', '¿Buscas una rutina de principiantes o avanzada?']
-rutinas = {
-    'Principiantes': ['Entrenamiento de cuerpo completo para principiantes', 'Rutina de cardio para principiantes'],
-    'Intermedios': ['Entrenamiento de fuerza para intermedios', 'Rutina de yoga para intermedios'],
-    'Avanzados': ['Entrenamiento de alta intensidad para avanzados', 'Rutina de pilates para avanzados']
-}
+sugerencias = ['¿Prefieres entrenamiento cardiovascular o de fuerza?', '¿Buscas una rutina de principiantes o avanzada?', '¿Quieres una rutina de fuerza o de cardio?']
+consejos = ['¿Quieres una rutina de fuerza o de cardio?', '¿Quieres una rutina de principiantes o avanzada?', '¿Quieres una rutina de fuerza o de cardio?']
+ejercicios = ['¿Quieres una rutina de fuerza o de cardio?', '¿Quieres una rutina de principiantes o avanzada?', '¿Quieres una rutina de fuerza o de cardio?']
+rutina_fuerza = ['4 repeticiones de 10 series de sentadillas', '4 repeticiones de 10 series de press de banca', '4 repeticiones de 10 series de press militar', '4 repeticiones de 10 series de remo con barra', '4 repeticiones de 10 series de dominadas', '4 repeticiones de 10 series de press de hombros', '4 repeticiones de 10 series de press de triceps', '4 repeticiones de 10 series de press de biceps', '4 repeticiones de 10 series de press de piernas', '4 repeticiones de 10 series de press de gemelos']
+rutina_cardio = {'10 minutos de bicicleta estatica', '10 minutos de caminadora', '10 minutos de eliptica', '10 minutos de remo', '10 minutos de escaladora', '10 minutos de natacion', '10 minutos de saltar la cuerda', '10 minutos de correr', '10 minutos de patinar', '10 minutos de andar en bicicleta'}
+# rutinas = {
+#     'Principiantes': ['Entrenamiento de cuerpo completo para principiantes', 'Rutina de cardio para principiantes'],
+#     'Intermedios': ['Entrenamiento de fuerza para intermedios', 'Rutina de yoga para intermedios'],
+#     'Avanzados': ['Entrenamiento de alta intensidad para avanzados', 'Rutina de pilates para avanzados']
+# }
 
 def procesar_entrada(entrada):
     lematizador = WordNetLemmatizer()
