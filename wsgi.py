50e8d4a21a13b1c7d0a4dac74a0fa774dd08ade2
mzfdev <54407007+mzfdev@users.noreply.github.com>
2023-07-04 17:37:49 -0600

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,4 @@
 from chatbot import app
-from waitress import serve
 
-if __name__ == "__main__":
-    serve(app, host='0.0.0.0', port=8000)
+def handler(event, context):
+    return app(event, context)
